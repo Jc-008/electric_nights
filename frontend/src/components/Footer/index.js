723cefa68c21f364e1fe2d55b404cc8c511b704f
jonathan <jonathanchan.808@gmail.com>
2021-04-29 11:41:25 -0400

    Created footer folder and file to work in

---
+++ frontend/src/components/Footer/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+function Footer(){
+
+
+  return (
+    <nav>
+      <div>
+        <a className="footer" href={''} style={{ marginLeft: '20px' }}>Github Profile</a>
+        <a className="footer" href={''} style={{ marginLeft: '20px' }}> Electric Nights Wiki</a>
+      </div>
+    </nav>
+  );
+}
+
+export default Footer;
