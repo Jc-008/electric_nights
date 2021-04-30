c58be64e30314c791d27b5102c8547bae7bc0a89
jonathan <jonathanchan.808@gmail.com>
2021-04-30 16:19:21 -0400

    Got register button on page with conditional logic

--- backend/routes/api/events.js
+++ backend/routes/api/events.js
@@ -2,16 +2,36 @@ const router = require('express').Router();
 const express = require('express')
 const asyncHandler = require('express-async-handler');
 const { setTokenCookie, requireAuth, restoreUser } = require('../../utils/auth');
-const { Event, Category, User } = require('../../db/models');                                // will need to add additional models
+const { Event, Category, User, UserEvent } = require('../../db/models');                                // will need to add additional models
 
 
+// ------ GET ---------------------------------------------------------------------------//
 
-// localhost:5000/ - homepage, will load if NO issues
+
+// localhost:/3000 - homepage, will load if NO issues
 router.get('/', asyncHandler( async(req, res) => {
-  const events = await Event.findAll({
+  const events = await Event.findAll({                // finding all events
     include: {model:User}
-  })    // finding all events
+  })
   res.json(events)                        //
 }))
+
+
+
+
+// ------- post------------------------------------------------------------------------//
+
+// //localhost:3000 - will need to add an new instance
+// router.post('/:id', asyncHandler( async(req, res) => {
+//   const {userId, eventId } = req.body
+//   console.log(userId)
+//   console.log(eventId)
+
+
+
+
+//   res.json(events)                        //
+// }))
+
 
 module.exports = router;
