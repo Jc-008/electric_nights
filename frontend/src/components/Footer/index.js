4479857c75e176b42433497463c0e0f61880ddc6
jonathan <jonathanchan.808@gmail.com>
2021-07-06 15:38:17 -0400

    Working on footer

--- frontend/src/components/Footer/index.js
+++ frontend/src/components/Footer/index.js
@@ -1,18 +1,24 @@
 import React from 'react';
-import {Link} from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import './Footer.css';
 
-function Footer(){
+function Footer() {
 
 
   return (
     <nav>
       <div className='footer-container'>
-        <div className='gitHub-div'>
-          <i className="fab fa-github"></i>
+        <div className='footer-left-container'>
           <a className='gitHub-text' target='_blank' href="https://github.com/Jc-008">GitHub</a>
         </div>
-        <a className="wiki-text" target='_blank' href="https://github.com/Jc-008/electric_nights/wiki">Electric Nights Wiki</a>
+        <div className='footer-right-container'>
+          <a className="wiki-text" target='_blank'>
+            © Electric Nights
+          </a>
+          <a className="wiki-text" target='_blank' href="https://github.com/Jc-008/electric_nights/wiki">
+            <i className="fab fa-github"></i>
+          </a>
+        </div>
       </div>
     </nav>
   );
@@ -22,5 +28,5 @@ function Footer(){
 export default Footer;
 
 
-{/* <a className="footer" target='_blank' href="https://github.com/Jc-008" style={{ marginLeft: '20px' }}>Github</a> */}
-{/* <a className="footer" target='_blank' href="https://github.com/Jc-008/electric_nights/wiki" style={{ marginLeft: '20px' }}>EN Wiki</a> */}
+{/* <a className="footer" target='_blank' href="https://github.com/Jc-008" style={{ marginLeft: '20px' }}>Github</a> */ }
+{/* <a className="footer" target='_blank' href="https://github.com/Jc-008/electric_nights/wiki" style={{ marginLeft: '20px' }}>EN Wiki</a> */ }
