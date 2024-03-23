7878a0dd79c808978089b21b278464e2eb41b7de
Jc-008 <jonathanchan.808@gmail.com>
2024-03-23 00:18:55 -0400

    updated backend routes index.js file

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,19 +1,8 @@
 // backend/routes/index.js
-
-
-// In this file we are creating an express ROUTER, a test Route and export the router at the bottom
-
-
 const express = require('express');
 const router = express.Router();
-const apiRouter = require('./api');                 // import and connecting router
-
-router.use('/api', apiRouter);    // using the api router and routes will start with /api/ from here
-
-
-/* ADDED  IN WHEN WORKING ON FRONT END:---------------------------------------------------------------------------------------------
-
-*/
+const apiRouter = require('./api');
+router.use('/api', apiRouter);
 
 // Static routes
 // Serve React build files in production
@@ -22,40 +11,29 @@ if (process.env.NODE_ENV === 'production') {
   // Serve the frontend's index.html file at the root route
   router.get('/', (req, res) => {
     res.cookie('XSRF-TOKEN', req.csrfToken());
-    return res.sendFile(
+    res.sendFile(
       path.resolve(__dirname, '../../frontend', 'build', 'index.html')
     );
   });
 
   // Serve the static assets in the frontend's build folder
   router.use(express.static(path.resolve("../frontend/build")));
-
   // Serve the frontend's index.html file at all other routes NOT starting with /api
   router.get(/^(?!\/?api).*/, (req, res) => {
     res.cookie('XSRF-TOKEN', req.csrfToken());
-    return res.sendFile(
+    res.sendFile(
       path.resolve(__dirname, '../../frontend', 'build', 'index.html')
     );
   });
 }
-
-
-/*
-  In development, you need another way to get the XSRF-TOKEN cookie on your frontend application because the React frontend is on a different server than the Express backend.
-    To solve this, add a backend route, GET /api/csrf/restore in the same file that can be accessed only in development and will restore the XSRF-TOKEN cookie.
-*/
 
 // Add a XSRF-TOKEN cookie in development
 if (process.env.NODE_ENV !== 'production') {
   router.get('/api/csrf/restore', (req, res) => {
     res.cookie('XSRF-TOKEN', req.csrfToken());
     res.status(201).json({});
-    return res.json({});
   });
 }
-
-
-
 
 // TEST ROUTE: --------------------------------------------------------------------------------------------------------------------------------------
 router.get('/hello/world', function (req, res) {
